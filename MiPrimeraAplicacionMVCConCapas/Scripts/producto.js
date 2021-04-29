cd0621dbfd3df16ef63a86629c20acce3f901725
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-04-29 11:12:14 -0400

    lista Producto terminada

---
+++ MiPrimeraAplicacionMVCConCapas/Scripts/producto.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+
+    listarProductos();
+}
+
+function listarProductos() {
+
+    pintar({
+        url: "Producto/lista", id: "divTabla",
+        cabeceras: ["Id Producto", "Nombre producto","Nombre marca", "Precio","stock","Denominacion"],
+        propiedades: ["iidproducto", "nombreProducto","nombreMarca","precioVenta", "stock","denominacion"]
+    })
+
+}
+
+function Buscar() {
+
+
+}
