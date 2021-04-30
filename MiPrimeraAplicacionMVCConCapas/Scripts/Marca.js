efb69c52300489114651b219a9105ad618924ce9
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-04-30 00:22:25 -0400

    marca.js modificado

--- MiPrimeraAplicacionMVCConCapas/Scripts/Marca.js
+++ MiPrimeraAplicacionMVCConCapas/Scripts/Marca.js
@@ -8,16 +8,24 @@ function listarMarca() {
         url: "Marca/listarMarca", id: "divTabla",
         cabeceras: ["Idmarca", "Nombre marca", "Descripcion"],
         propiedades: ["idMarca", "nombreMarca", "descripcion"]
+    }, {
+        busqueda: true,
+         url: "Marca/FiltrarMarca",
+        nombreParametro: "nombre",
+        id: "txtNombreCama",
+        placeholder: "Ingrese nombre de la marca",
+        button: false,
+        type: "text"
     })       
 }
 
-function Buscar() {
-    var nombreMarca = get("txtNombreMarca");
-    pintar({
-        url: "Marca/FiltrarMarca/?nombre=" + nombreMarca,
-        id: "divTabla",
-        cabeceras: ["Idmarca", "Nombre marca", "Descripcion"],
-        propiedades: ["idMarca", "nombreMarca", "descripcion"]
-    })
+//function Buscar() {
+//    var nombreMarca = get("txtNombreMarca");
+//    pintar({
+//        url: "Marca/FiltrarMarca/?nombre=" + nombreMarca,
+//        id: "divTabla",
+//        cabeceras: ["Idmarca", "Nombre marca", "Descripcion"],
+//        propiedades: ["idMarca", "nombreMarca", "descripcion"]
+//    })
 
-}
+//}
